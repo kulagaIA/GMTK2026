675351731b6faa53016dd99c0ef7029603b91302
Denis Maximenko <manlok71@gmail.com>
2026-07-25 19:44:49 +0400

    Implement camera transitions

--- Core/States/gameplay_game_state.gd
+++ Core/States/gameplay_game_state.gd
@@ -6,6 +6,11 @@ func enter(prev_state : State) -> void:
 	super.enter(prev_state)
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	assert(Game.gameplay)
+	var camera := SmashCamera.get_active()
+	var target_camera := SmashCamera.get_by_tag(SmashCamera.Tag.GAMEPLAY)
+	assert(camera)
+	assert(target_camera)
+	await camera.fly_and_reparent(target_camera, 2.5)
 	Game.gameplay.hit_occurred.connect(Game.player_state._on_hit_occurred)
 	Game.player_state.health.value_changed.connect(Game.player._on_health_value_changed)
 	Game.gameplay.restart_gameplay()
