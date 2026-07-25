6b7901063be93e28f7838177d779005807ef094a
IvanKulaga <kulagaivanandreevich@gmail.com>
2026-07-26 00:37:59 +0500

    enable damage multiplier for head movement + round_countdown hat display fix

--- UI/Info/round_countdown.gd
+++ UI/Info/round_countdown.gd
@@ -4,6 +4,10 @@ extends Control
 @onready var countdown_label: Label = $CountdownLabel
 @onready var timer: Timer = $Timer
 
+func _ready() -> void:
+	Game.gameplay.player.face_renderer.init_hat()
+
+
 func _process(delta: float) -> void:
 	countdown_label.text = "%d..." % [int(ceilf(timer.time_left))]
 
