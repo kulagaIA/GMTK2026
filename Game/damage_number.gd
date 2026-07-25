535aef758ff154d3fd9c5f7024560eb43808ad70
Denis Maximenko <manlok71@gmail.com>
2026-07-25 20:23:48 +0400

    Tweak damage number flight path

--- Game/damage_number.gd
+++ Game/damage_number.gd
@@ -2,29 +2,29 @@ class_name DamageNumber
 extends Node3D
 
 @export var duration: float = 2
-@export var speed: float = 10
+@export var speed: float = 5
 @export var speed_deviation: float = 3
 @export var size: float = .5
-@export var angle_deviation: float = .5
+@export var angle_deviation_deg: float = 15.0
 var damage_value: int
 var is_crit: bool
 var velocity: Vector3
 
-func _enter_tree() -> void:
-	%Label.text = str(damage_value)
+@onready var label: Label3D = %Label
+
+func _ready() -> void:
+	label.text = str(damage_value)
 	if is_crit:
-		%Label.modulate = Color.DARK_RED
+		label.modulate = Color.DARK_RED
 	velocity = Vector3.ZERO
 	velocity.y = randfn(speed, speed_deviation)
-	velocity = velocity.rotated(Vector3.FORWARD, randfn(0, angle_deviation))
-	velocity = velocity.rotated(Vector3.LEFT, randfn(0, angle_deviation))
-	print(velocity)
-	%Label.scale = Vector3(size, size, size)
+	velocity = velocity.rotated(Vector3.FORWARD, deg_to_rad(randfn(0, 15.0)))
+	velocity = velocity.rotated(Vector3.RIGHT, deg_to_rad(randfn(-5.0, 15.0)))
+	label.scale = Vector3(size, size, size)
 
 func _process(delta: float) -> void:
 	duration -= delta
 	if (duration <= 0):
 		self.queue_free()
 	
-	global_position = global_position.move_toward(velocity, delta)
-	%Label.basis
+	global_position += velocity * delta
