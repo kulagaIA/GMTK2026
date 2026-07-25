a09887572f19d045dc4ac55cf3698a4812007c0f
Zorik <zorikwot@gmail.com>
2026-07-25 16:24:00 +0600

    small fix for damage numbers

--- Game/damage_number.gd
+++ Game/damage_number.gd
@@ -5,20 +5,21 @@ extends Node3D
 @export var speed: float = 10
 @export var speed_deviation: float = 3
 @export var size: float = .5
-@export var angle_deviation: float = 1
+@export var angle_deviation: float = .5
 var damage_value: int
 var is_crit: bool
 var velocity: Vector3
 
 func _enter_tree() -> void:
-	$Label3D.text = str(damage_value)
+	%Label.text = str(damage_value)
 	if is_crit:
-		$Label3D.modulate = Color.DARK_RED
+		%Label.modulate = Color.DARK_RED
 	velocity = Vector3.ZERO
 	velocity.y = randfn(speed, speed_deviation)
-	velocity.rotated(Vector3.FORWARD, randfn(0, angle_deviation))
-	velocity.rotated(Vector3.LEFT, randfn(0, angle_deviation))
-	$Label3D.scale = Vector3(size, size, size)
+	velocity = velocity.rotated(Vector3.FORWARD, randfn(0, angle_deviation))
+	velocity = velocity.rotated(Vector3.LEFT, randfn(0, angle_deviation))
+	print(velocity)
+	%Label.scale = Vector3(size, size, size)
 
 func _process(delta: float) -> void:
 	duration -= delta
@@ -26,4 +27,4 @@ func _process(delta: float) -> void:
 		self.queue_free()
 	
 	global_position = global_position.move_toward(velocity, delta)
-	$Label3D.basis
+	%Label.basis
