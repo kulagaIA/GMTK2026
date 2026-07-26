05ea005af60a25c0b3d499899e803ff1280b7968
Denis Maximenko <manlok71@gmail.com>
2026-07-26 17:14:18 +0400

    Fix beer softlock

--- Game/player.gd
+++ Game/player.gd
@@ -263,11 +263,12 @@ var _drinking_pivo: bool = false
 
 func _start_drinking_pivo() -> void:
 	pivo_path_follow.progress_ratio = 0
-	var tween: Tween = get_tree().create_tween()
 	_reset_neck(drinking_time / 2 - .2)
-	await tween.tween_property(pivo_path_follow, "progress_ratio", 1, drinking_time / 2).set_ease(Tween.EASE_IN_OUT).finished
-	tween = get_tree().create_tween()
-	await tween.tween_property(pivo_path_follow, "progress_ratio", 0, drinking_time / 2).set_ease(Tween.EASE_IN_OUT).finished
+	var tween: Tween = get_tree().create_tween()
+	tween.tween_property(pivo_path_follow, "progress_ratio", 1, drinking_time / 2).set_ease(Tween.EASE_IN_OUT)
+	tween.tween_property(pivo_path_follow, "progress_ratio", 0, drinking_time / 2).set_ease(Tween.EASE_IN_OUT)
+	await tween.finished
+	_drinking_pivo = false
 	player_state.pivo_charges.add_modifier(AttributeModInfo.new(AttributeModInfo.ModType.ADD_FLAT, -1))
 	player_state.pivo.activate()
 
