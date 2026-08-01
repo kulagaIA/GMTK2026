f442cc5cb712153457e1bff5f196dc05b4013e86
Denis Maximenko <manlok71@gmail.com>
2026-08-01 17:24:01 +0400

    Rework hurt vignette

--- UI/HUD/hud.gd
+++ UI/HUD/hud.gd
@@ -1,12 +1,19 @@
 class_name HUD
 extends Control
 
+var health : Attribute:
+	get:
+		return Game.player_state.health
+
+var max_health : Attribute:
+	get:
+		return Game.player_state.max_health
 
 func _ready() -> void:
 	Game.player.stun_status_changed.connect(_on_player_stunned)
 	var texture := Game.player.face_renderer.subviewport.get_texture() as ViewportTexture
 	set_face_texture(texture)
-	Game.player_state.health.value_changed.connect(_on_health_value_changed)
+	health.value_changed.connect(_on_health_value_changed)
 
 func _process(delta: float) -> void:
 	pass
@@ -22,19 +29,29 @@ func _on_player_stunned(stunned: bool) -> void:
 
 #region PP
 
-@export var vignette_threshold: float = 60
-@export var vignette_color: Color = Color.DARK_RED
-const vignette_scene: PackedScene = preload("res://UI/PP/vignette.tscn")
-var vignette: Vignette = null
+@export var hurt_threshold_ratio: float = 0.3
+var hurt_threshold: float:
+	get:
+		return max_health.value * hurt_threshold_ratio
+
+const hurt_effect_scene: PackedScene = preload("uid://puarbl8g8rtt")
+var hurt_effect: Vignette = null
 
 func _update_vignette() -> void:
-	if vignette == null:
-		vignette = vignette_scene.instantiate() as Vignette
-		add_child(vignette)
-	vignette.update(clampf(1 - Game.player_state.health.value / vignette_threshold, 0, 1), vignette_color)
+	if not hurt_effect:
+		hurt_effect = hurt_effect_scene.instantiate() as Vignette
+		Game.canvas_manager.set_layer_content(JamUtils.layer_ui_post_process, hurt_effect)
+	hurt_effect.alpha = clampf(1 - health.value / hurt_threshold, 0, 1)
+
+func _remove_vignette() -> void:
+	if hurt_effect:
+		hurt_effect.queue_free()
+		hurt_effect = null
 
 func _on_health_value_changed(attribute : Attribute, new_value : float, old_value : float) -> void:
-	if new_value <= vignette_threshold:
+	if new_value <= hurt_threshold:
 		_update_vignette()
+	else:
+		_remove_vignette()
 
 #endregion
--- UI/PP/vignette.gd
+++ UI/PP/vignette.gd
@@ -1,14 +1,41 @@
+@tool
 class_name Vignette
-extends CanvasLayer
+extends Control
 
-@export var inner_radius: float = 0
-@export var outer_radius: float = 1
+@export var inner_radius: float = 0.0:
+	get:
+		return inner_radius
+	set(new_radius):
+		inner_radius = new_radius
+		_material.set_shader_parameter(&"inner_radius", inner_radius)
 
-func _process(delta: float) -> void:
-	pass
+@export var outer_radius: float = 1.0:
+	get:
+		return outer_radius
+	set(new_radius):
+		outer_radius = new_radius
+		_material.set_shader_parameter(&"outer_radius", outer_radius)
 
-func update(alpha: float, color: Color) -> void:
-	(%ColorRect.material as ShaderMaterial).set_shader_parameter("alpha", alpha)
-	(%ColorRect.material as ShaderMaterial).set_shader_parameter("color", color)
-	(%ColorRect.material as ShaderMaterial).set_shader_parameter("inner_radius", inner_radius)
-	(%ColorRect.material as ShaderMaterial).set_shader_parameter("outer_radius", outer_radius)
+@export var vignette_color: Color = Color.BLACK:
+	get:
+		return vignette_color
+	set(new_color):
+		vignette_color = new_color
+		_material.set_shader_parameter(&"color", vignette_color)
+
+@export_range(0.0, 1.0, 0.01) var alpha: float = 1.0:
+	get:
+		return alpha
+	set(new_alpha):
+		alpha = new_alpha
+		_material.set_shader_parameter(&"alpha", alpha)
+
+var _material : ShaderMaterial:
+	get:
+		return material as ShaderMaterial
+
+func _ready() -> void:
+	_material.set_shader_parameter(&"alpha", alpha)
+	_material.set_shader_parameter(&"color", vignette_color)
+	_material.set_shader_parameter(&"inner_radius", inner_radius)
+	_material.set_shader_parameter(&"outer_radius", outer_radius)
