439e1a605e7f409bc9145acb866bfad5fc73ae0b
Denis Maximenko <manlok71@gmail.com>
2026-08-08 21:05:00 +0400

    Fix game state deadlock

--- Core/States/round_over_game_state.gd
+++ Core/States/round_over_game_state.gd
@@ -4,9 +4,13 @@ extends GameState
 
 func enter(prev_state : State) -> void:
 	super.enter(prev_state)
-	Game.change_game_state(GameState.ROUND_OVER)
+	temp()
 	#Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	#Game.init_game_over(Game.StageResult.LOOSE)
+
+func temp() -> void:
+	await get_tree().create_timer(0.1).timeout
+	Game.change_game_state(GameState.PROGRESSION)
 
 func exit(next_state : State) -> void:
 	Game.canvas_manager.clear_layer(JamUtils.layer_ui_menu)
