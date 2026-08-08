237ea29a9194541321b6cc26675cf1b7657ddc1c
Denis Maximenko <manlok71@gmail.com>
2026-08-08 20:36:09 +0400

    Skip post-round state

--- Core/States/round_over_game_state.gd
+++ Core/States/round_over_game_state.gd
@@ -4,8 +4,9 @@ extends GameState
 
 func enter(prev_state : State) -> void:
 	super.enter(prev_state)
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	Game.init_game_over(Game.StageResult.LOOSE)
+	Game.change_game_state(GameState.ROUND_OVER)
+	#Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	#Game.init_game_over(Game.StageResult.LOOSE)
 
 func exit(next_state : State) -> void:
 	Game.canvas_manager.clear_layer(JamUtils.layer_ui_menu)
