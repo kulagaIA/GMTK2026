e067e23879ad26d29c5fffa2141026b4f95f5476
Denis Maximenko <manlok71@gmail.com>
2026-08-09 17:01:04 +0400

    Vary damage numbers' scale and color from amplitude

--- UI/HUD/DamageNumbers/damage_number_control.gd
+++ UI/HUD/DamageNumbers/damage_number_control.gd
@@ -4,16 +4,22 @@ extends Label
 @export var speed: float = 5
 @export var speed_deviation: float = 3
 @export var angle_deviation_deg: float = 15.0
-var damage_value: float
-var is_crit: bool
-var velocity: Vector2
+var damage_value : float = 1.0
+var is_crit : bool = false
+var amplitude : float = 0.5
+var velocity : Vector2
+@export var color_from_amplitude : Gradient = null
+@export var scale_from_amplitude : Curve = null
+@export var crit_color : Color = Color.ORANGE_RED
 
 func _ready() -> void:
 	text = str(int(damage_value))
 	if is_crit:
-		modulate = Color.ORANGE_RED
+		modulate = crit_color
 	else:
-		modulate = Color.YELLOW
+		modulate = color_from_amplitude.sample(amplitude)
+	scale.x = scale_from_amplitude.sample(amplitude)
+	scale.y = scale.x
 	velocity = Vector2.ZERO
 	velocity.y = -randfn(speed, speed_deviation)
 	velocity = velocity.rotated(deg_to_rad(randfn(0, angle_deviation_deg)))
